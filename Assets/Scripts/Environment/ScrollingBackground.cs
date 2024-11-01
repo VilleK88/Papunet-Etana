182fe9775db34f70406edda4301fa32717de628e
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2024-11-01 10:30:26 +0200

    Repeating background fix

--- Assets/Scripts/Environment/ScrollingBackground.cs
+++ Assets/Scripts/Environment/ScrollingBackground.cs
@@ -5,14 +5,38 @@ public class ScrollingBackground : MonoBehaviour
     float repeatWidth;
     [SerializeField] GameObject sprite1;
     [SerializeField] GameObject sprite2;
+    float repeatDistance;
+    float angle = 14f;
+    Vector2 movementDirection;
+    public bool horizontal = true;
     private void Start()
     {
         startPos = transform.position;
-        repeatWidth = sprite1.GetComponent<BoxCollider2D>().size.x / 2 + sprite2.GetComponent<BoxCollider2D>().size.x / 2;
+        if (horizontal) repeatWidth = sprite1.GetComponent<BoxCollider2D>().size.x / 2 + sprite2.GetComponent<BoxCollider2D>().size.x / 2;
+        else
+        {
+            float width1 = sprite1.GetComponent<BoxCollider2D>().size.x;
+            float width2 = sprite2.GetComponent<BoxCollider2D>().size.x;
+            float averageWidth = (width1 + width2) / 2;
+            float angleInRadians = angle * Mathf.Deg2Rad;
+            movementDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+            repeatWidth = averageWidth;
+        }
     }
     private void Update()
     {
-        if (transform.position.x < startPos.x - repeatWidth)
-            transform.position = startPos;
+        if (horizontal) HorizontalRepeat();
+        else RepeatWithAngle();
+    }
+    void HorizontalRepeat()
+    {
+        if (transform.position.x < startPos.x - repeatWidth) transform.position = startPos;
+    }
+    void RepeatWithAngle()
+    {
+        Vector2 currentPos = transform.position;
+        float distance = Vector2.Distance(startPos, currentPos);
+        if (distance >= repeatWidth) transform.position = startPos;
+        else transform.position = currentPos + movementDirection * Time.deltaTime;
     }
 }
